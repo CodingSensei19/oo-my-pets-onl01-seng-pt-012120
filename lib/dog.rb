5063166725a225b59552d7087054dbb495f0a7ef
Aaron Wilson <codingsensei19@gmail.com>
2020-02-14 00:14:00 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,16 @@
 class Dog
-  # code goes here
+attr_accessor :mood, :Owner
+attr_reader :name
+@@all = []
+
+def initialize(name, owner)
+   @name = name
+   @mood = "nervous"
+  @owner = owner
+  Owner.dogs << self
+end
+
+def self.all
+  @@all
+  end
 end
